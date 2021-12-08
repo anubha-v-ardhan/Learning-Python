8c19b40a559ffff8a93dec9b085013e974718b2b
Anubhav Vardhan <vardhananubhav@gmail.com>
2021-12-08 16:47:32 +0530

    intial commit

---
+++ 002-String-Concatenation.py
@@ -0,0 +1,3 @@
+# String Concatenation => Adding Strings Together
+
+print("20 Days are " + str(50) + " minutes")  # In python, we need to change no. to String ourselves like this
---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hello World")
+print(-20)
+
+# Data Types
+
+# String
+# Text: String (str) [we can use both '' and "" for String in python]
+# Whole numbers, positive or negative, without decimals: Integer (Int)
+# Positive or negative, contains one or more decimals: Floating point numbers (Float)
+
+print(3.5)  # Float
+print(3)    # Int
+
+# Minuted in 20 Days
+print(20 * 24 * 60)
